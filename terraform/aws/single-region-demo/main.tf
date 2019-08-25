5f092a56e3a7a1d530c8f830647988fad338c474
Yulei Liu <yulei.liu@gmail.com>
2019-08-25 19:24:39 +1000

    minor changes

--- terraform/aws/single-region-demo/main.tf
+++ terraform/aws/single-region-demo/main.tf
@@ -3,18 +3,15 @@
 # Create Consult Connect demo cluster
 module "cluster_main" {
   source = "../modules/consul-demo-cluster"
-
   aws_region    = "${var.aws_region}"
   consul_dc     = "${var.consul_dc}"
   consul_acl_dc = "${var.consul_dc}"
 
   project_name     = "${var.project_name}"
   top_level_domain = "${var.top_level_domain}"
-
   route53_zone_id = "${var.route53_zone_id}"
   ssh_key_name    = "${var.ssh_key_name}"
   consul_lic      = "${var.consul_lic}"
-
   hashi_tags = "${var.hashi_tags}"
 }
 
@@ -29,7 +26,6 @@ resource "consul_prepared_query" "product_service" {
   name         = "product"
   only_passing = true
   connect      = true
-
   service = "product"
 
   failover {
